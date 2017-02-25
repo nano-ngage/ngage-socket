bc928957243195349d2a6c0e06b1cf74440c6e36
Saivickna Raveendran <saivickna.r@gmail.com>
2017-02-25 10:26:25 -0800

    (test) Added additional tests for sockets

--- spec/socket-spec.js
+++ spec/socket-spec.js
@@ -1,6 +1,7 @@
 var io = require('socket.io-client');
-
-var socketURL = 'http://localhost:5500/ngage';
+var app = require('../server/server.js');
+var expect = require('chai').expect;
+var socketURL = 'http://localhost:4568/ngage';
 
 var options ={
   transports: ['websocket'],
@@ -9,12 +10,22 @@ var options ={
 };
 
 describe("Chat Server",function(){
+  var server;
+  var client1, client2;
+  before(function() {
+    server = app.listen(4568, function() {
+      console.log('Server is listening on 4568');
+    });
+    client1 = io.connect(socketURL, options);
+    client2 = io.connect(socketURL, options);
+  });
+  after(function() {
+    server.close();
+  });
   it('Should broadcast new user to all users', function(done){
     this.timeout(5000);
-    var client1 = io.connect(socketURL, options);
     client1.emit('subscribe', {room: 'channel'});
     client1.on('join', data => {
-      var client2 = io.connect(socketURL, options);
       client2.emit('subscribe', {room: 'channel'});   
       client2.on('join', data => {
         client1.emit('start', {room: 'channel'});
@@ -22,6 +33,22 @@ describe("Chat Server",function(){
           done();
         })
       })   
+    })
+  });
+  it('Should receive askQ socket and receive a server is unavailable message', function(done){
+    this.timeout(5000);
+    client1.emit('askQ', {question: {questionID:5}});
+    client1.on('answers', data => {
+      expect(data).to.equal('Server is unavailable');
+      done();
+    })
+  });
+  it('Should receive submitResponse socket and receive a server is unavailable message', function(done){
+    this.timeout(5000);
+    client1.emit('submitResponse', 'Test Data');
+    client1.on('responseSubmit', data => {
+      expect(data).to.equal('Server is unavailable');
+      done();
     })
   });
 });
