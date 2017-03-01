5568319391967b0044bfc9468370f310ec9d4192
Jordan Medina <jordanwmedina@gmail.com>
2017-03-01 10:23:24 -0800

    (cleanup) Clean socket code to be more readable

--- server/server.js
+++ server/server.js
@@ -22,31 +22,39 @@ nsp.on('connection', function (socket) {
   socket.on('subscribe', data => {
     socket.join(data.room);
     socket.emit('join');
-  })
-  socket.on('unsubscribe', function(data) { socket.leave(data.room); })
-  socket.on('start', function (data) {
-    //socket.broadcast.to(data.room).emit('start');
-    nsp.in(data.room).emit('start', '');
-    fetch(url + 'qByS/' + data.room).then(res => res.json()).then(questions => {
-      nsp.in(data.room).emit('questions', questions);
-    }).catch (e => {
-      socket.emit('questions', 'Server is unavailable');
-    })
   });
-  socket.on('askQ', function (data) {
-    fetch(url + 'aByQ/' + data.question.questionID).then(handleErrors).then(res => res.json()).then(answers => {
-      nsp.in(data.room).emit('answers', {answers: answers, question: data.question});
-    }).catch(e => {
-      socket.emit('answers', 'Server is unavailable');
-    })
 
+  socket.on('unsubscribe', function(data) { socket.leave(data.room); })
+
+  socket.on('start', function (data) {
+    nsp.in(data.room).emit('start', '');
+
+    fetch(url + 'qByS/' + data.room)
+      .then(res => res.json())
+      .then(questions => { nsp.in(data.room).emit('questions', questions); })
+      .catch (e => { socket.emit('questions', 'Server is unavailable'); });
   });
+
+  socket.on('askQ', function (data) {
+    fetch(url + 'aByQ/' + data.question.questionID)
+      .then(handleErrors)
+      .then(res => res.json())
+      .then(answers => { nsp.in(data.room).emit('answers', {answers: answers, question: data.question}); })
+      .catch(e => { socket.emit('answers', 'Server is unavailable'); })
+  });
+
   socket.on('submitResponse', data => {
     nsp.in(data.room).emit('resp', data);
-    fetch(url + 'r', { method: 'POST', headers: {"Content-Type": "application/json"}, mode: 'cors',body: JSON.stringify(data) }).catch(e => {
-      socket.emit('responseSubmit', 'Server is unavailable');
-    });
+
+    fetch(url + 'r', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        mode: 'cors',
+        body: JSON.stringify(data)
+      })
+      .catch(e => { socket.emit('responseSubmit', 'Server is unavailable'); });
   });
+
 });
 
 module.exports = server;
